27e2edf0882bd5ad3aaaa51f986dd53748772bdf
loveyana <87516818+loveyana@users.noreply.github.com>
2022-03-18 01:28:21 +0800

    proto/agent: add JWT SVID privileged API (#20)
    * proto/agent: add JWT SVID privileged API
    
    Signed-off-by: Yuhan Li <liyuhan.loveyana@bytedance.com>
    
    * Fix comment lint problem
    
    Signed-off-by: Yuhan Li <liyuhan.loveyana@bytedance.com>

--- proto/spire/api/agent/delegatedidentity/v1/delegatedidentity_grpc.pb.go
+++ proto/spire/api/agent/delegatedidentity/v1/delegatedidentity_grpc.pb.go
@@ -23,6 +23,12 @@ type DelegatedIdentityClient interface {
 	// Subscribe to get local and all federated bundles.
 	// The lifetime of the subscription aligns to the lifetime of the stream.
 	SubscribeToX509Bundles(ctx context.Context, in *SubscribeToX509BundlesRequest, opts ...grpc.CallOption) (DelegatedIdentity_SubscribeToX509BundlesClient, error)
+	// Fetch JWT-SVIDs for workloads that match the given selectors, and
+	// for the requested audience.
+	FetchJWTSVIDs(ctx context.Context, in *FetchJWTSVIDsRequest, opts ...grpc.CallOption) (*FetchJWTSVIDsResponse, error)
+	// Subscribe to get local and all federated JWKS bundles.
+	// The lifetime of the subscription aligns to the lifetime of the stream.
+	SubscribeToJWTBundles(ctx context.Context, in *SubscribeToJWTBundlesRequest, opts ...grpc.CallOption) (DelegatedIdentity_SubscribeToJWTBundlesClient, error)
 }
 
 type delegatedIdentityClient struct {
@@ -97,6 +103,47 @@ func (x *delegatedIdentitySubscribeToX509BundlesClient) Recv() (*SubscribeToX509
 	return m, nil
 }
 
+func (c *delegatedIdentityClient) FetchJWTSVIDs(ctx context.Context, in *FetchJWTSVIDsRequest, opts ...grpc.CallOption) (*FetchJWTSVIDsResponse, error) {
+	out := new(FetchJWTSVIDsResponse)
+	err := c.cc.Invoke(ctx, "/spire.api.agent.delegatedidentity.v1.DelegatedIdentity/FetchJWTSVIDs", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *delegatedIdentityClient) SubscribeToJWTBundles(ctx context.Context, in *SubscribeToJWTBundlesRequest, opts ...grpc.CallOption) (DelegatedIdentity_SubscribeToJWTBundlesClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_DelegatedIdentity_serviceDesc.Streams[2], "/spire.api.agent.delegatedidentity.v1.DelegatedIdentity/SubscribeToJWTBundles", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &delegatedIdentitySubscribeToJWTBundlesClient{stream}
+	if err := x.ClientStream.SendMsg(in); err != nil {
+		return nil, err
+	}
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	return x, nil
+}
+
+type DelegatedIdentity_SubscribeToJWTBundlesClient interface {
+	Recv() (*SubscribeToJWTBundlesResponse, error)
+	grpc.ClientStream
+}
+
+type delegatedIdentitySubscribeToJWTBundlesClient struct {
+	grpc.ClientStream
+}
+
+func (x *delegatedIdentitySubscribeToJWTBundlesClient) Recv() (*SubscribeToJWTBundlesResponse, error) {
+	m := new(SubscribeToJWTBundlesResponse)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 // DelegatedIdentityServer is the server API for DelegatedIdentity service.
 // All implementations must embed UnimplementedDelegatedIdentityServer
 // for forward compatibility
@@ -107,6 +154,12 @@ type DelegatedIdentityServer interface {
 	// Subscribe to get local and all federated bundles.
 	// The lifetime of the subscription aligns to the lifetime of the stream.
 	SubscribeToX509Bundles(*SubscribeToX509BundlesRequest, DelegatedIdentity_SubscribeToX509BundlesServer) error
+	// Fetch JWT-SVIDs for workloads that match the given selectors, and
+	// for the requested audience.
+	FetchJWTSVIDs(context.Context, *FetchJWTSVIDsRequest) (*FetchJWTSVIDsResponse, error)
+	// Subscribe to get local and all federated JWKS bundles.
+	// The lifetime of the subscription aligns to the lifetime of the stream.
+	SubscribeToJWTBundles(*SubscribeToJWTBundlesRequest, DelegatedIdentity_SubscribeToJWTBundlesServer) error
 	mustEmbedUnimplementedDelegatedIdentityServer()
 }
 
@@ -119,6 +172,12 @@ func (UnimplementedDelegatedIdentityServer) SubscribeToX509SVIDs(*SubscribeToX50
 }
 func (UnimplementedDelegatedIdentityServer) SubscribeToX509Bundles(*SubscribeToX509BundlesRequest, DelegatedIdentity_SubscribeToX509BundlesServer) error {
 	return status.Errorf(codes.Unimplemented, "method SubscribeToX509Bundles not implemented")
+}
+func (UnimplementedDelegatedIdentityServer) FetchJWTSVIDs(context.Context, *FetchJWTSVIDsRequest) (*FetchJWTSVIDsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method FetchJWTSVIDs not implemented")
+}
+func (UnimplementedDelegatedIdentityServer) SubscribeToJWTBundles(*SubscribeToJWTBundlesRequest, DelegatedIdentity_SubscribeToJWTBundlesServer) error {
+	return status.Errorf(codes.Unimplemented, "method SubscribeToJWTBundles not implemented")
 }
 func (UnimplementedDelegatedIdentityServer) mustEmbedUnimplementedDelegatedIdentityServer() {}
 
@@ -175,10 +234,54 @@ func (x *delegatedIdentitySubscribeToX509BundlesServer) Send(m *SubscribeToX509B
 	return x.ServerStream.SendMsg(m)
 }
 
+func _DelegatedIdentity_FetchJWTSVIDs_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(FetchJWTSVIDsRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(DelegatedIdentityServer).FetchJWTSVIDs(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/spire.api.agent.delegatedidentity.v1.DelegatedIdentity/FetchJWTSVIDs",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(DelegatedIdentityServer).FetchJWTSVIDs(ctx, req.(*FetchJWTSVIDsRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _DelegatedIdentity_SubscribeToJWTBundles_Handler(srv interface{}, stream grpc.ServerStream) error {
+	m := new(SubscribeToJWTBundlesRequest)
+	if err := stream.RecvMsg(m); err != nil {
+		return err
+	}
+	return srv.(DelegatedIdentityServer).SubscribeToJWTBundles(m, &delegatedIdentitySubscribeToJWTBundlesServer{stream})
+}
+
+type DelegatedIdentity_SubscribeToJWTBundlesServer interface {
+	Send(*SubscribeToJWTBundlesResponse) error
+	grpc.ServerStream
+}
+
+type delegatedIdentitySubscribeToJWTBundlesServer struct {
+	grpc.ServerStream
+}
+
+func (x *delegatedIdentitySubscribeToJWTBundlesServer) Send(m *SubscribeToJWTBundlesResponse) error {
+	return x.ServerStream.SendMsg(m)
+}
+
 var _DelegatedIdentity_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "spire.api.agent.delegatedidentity.v1.DelegatedIdentity",
 	HandlerType: (*DelegatedIdentityServer)(nil),
-	Methods:     []grpc.MethodDesc{},
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "FetchJWTSVIDs",
+			Handler:    _DelegatedIdentity_FetchJWTSVIDs_Handler,
+		},
+	},
 	Streams: []grpc.StreamDesc{
 		{
 			StreamName:    "SubscribeToX509SVIDs",
@@ -188,6 +291,11 @@ var _DelegatedIdentity_serviceDesc = grpc.ServiceDesc{
 		{
 			StreamName:    "SubscribeToX509Bundles",
 			Handler:       _DelegatedIdentity_SubscribeToX509Bundles_Handler,
+			ServerStreams: true,
+		},
+		{
+			StreamName:    "SubscribeToJWTBundles",
+			Handler:       _DelegatedIdentity_SubscribeToJWTBundles_Handler,
 			ServerStreams: true,
 		},
 	},
